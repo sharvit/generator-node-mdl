2d84e9afc6d181ffd1e8b8a3693b66cddfd09f5a
Avi Sharvit <sharvita@gmail.com>
2019-07-28 09:51:24 +0300

    feat(interface): adds informative messages to the user

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import YeomanGenerator from 'yeoman-generator';
+import chalk from 'chalk';
 
 import './generators/project';
 import './generators/github';
@@ -24,5 +25,20 @@ export default class NodeMdlGenerator extends YeomanGenerator {
     if (this.props.travisCI) {
       this.composeWith(require.resolve('./generators/travis'), this.props);
     }
+  }
+
+  writing() {
+    this.log('\n\n\n');
+    this.log(`${chalk.underline.bold('Writing files to disk:')}\n`);
+  }
+
+  install() {
+    this.log('\n\n\n');
+    this.log(`${chalk.underline.bold('Installing:')}\n`);
+  }
+
+  end() {
+    this.log('\n\n\n');
+    this.log(`${chalk.underline.bold('Finishing:')}\n`);
   }
 }
